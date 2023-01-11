e1c68d87253a1ccc7612028657062c6efbcd97d6
A-naima <naimaabdou148@gmail.com>
2023-01-11 13:43:42 +0100

    v.1

---
+++ ex1.py
@@ -0,0 +1,5 @@
+a = int(input("Entrez la valeur de N :"))
+somme = 0
+for i in range(n+1):
+    somme += i
+    print ("La )
---
+++ tp3/exo1A.py
@@ -0,0 +1,5 @@
+a = int(input("Entrez la valeur de N :"))
+somme = 0
+for i in range(a+1):
+    somme += i
+print("La somme des entiers de 0 à", a, "est", somme)
